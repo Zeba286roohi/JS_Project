dee6dada9718227a0cfa49eaaf583553e42b3289
Zeba <85834320+Zeba286roohi@users.noreply.github.com>
2023-12-22 15:11:46 +0530

    Delete Crypto/zeba directory

--- Crypto/zeba/projects related/mailer/mailer/routes/route.js
+++
@@ -1,6 +0,0 @@
-const router=require('express').Router();
-const {singnup,getbill}=require('../controller/appController')
-router.post('/user/singnUp',singnup)
-router.post('/product/getBill',getbill)
-
-module.exports=router;
